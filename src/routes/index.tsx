cbebbe15c273e30befe1d8e71cb95515c5b797c5
Leticia Borges <102327509+leticiaborgess@users.noreply.github.com>
2022-07-05 20:18:11 -0300

    navegação drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,49 @@
 import { Cadastro } from "../screens/Cadastro";
 import { Login } from "../screens/Login";
+import { Home } from "../screens/Home";
+import { Logout } from "../screens/Logout";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Home } from "../screens/Home";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 
 const Stack = createStackNavigator();
 
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName='Cadastro'
+      screenOptions={{
+        headerShown: false
+      }}>
+      <Stack.Screen name='Cadastro' component={Cadastro} />
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Home' component={MyDrawer} />
+    </Stack.Navigator>
+  )
+}
+
+const Drawer = createDrawerNavigator();
+function MyDrawer() {
+  return (
+
+    <Drawer.Navigator
+    screenOptions={{
+      headerShown: false,
+      drawerPosition: "right",
+      drawerActiveBackgroundColor: '#FFE9B1',
+      
+    }}>
+      <Drawer.Screen name="Main" component={Home} />
+      <Drawer.Screen name="Logout" component={Logout} />
+    </Drawer.Navigator>
+  );
+}
 export const Rotas = () => {
   return (
     <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName='Cadastro'
-                screenOptions={{
-                    headerShown: false
-                }}>
-                <Stack.Screen name='Cadastro' component={Cadastro}/>
-                <Stack.Screen name='Login' component={Login}/>
-                <Stack.Screen name='Home' component={Home}/>
-                
-            </Stack.Navigator>
-        </NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   )
 }
-    
