4e174e2c5024ecdd5114d43dfeb590dd1709f6ec
Samir Jaber Copar <samirjcop@gmail.com>
2022-07-07 14:57:48 -0300

    adiciona link para receita na página de favoritos

--- src/screens/Favoritos/index.tsx
+++ src/screens/Favoritos/index.tsx
@@ -4,15 +4,24 @@ import { Header } from "../../components/Header";
 import { ReceitaCard } from "../../components/ReceitaCard";
 import { UserInfoContext } from "../../context/UserInfoContext";
 
-export const Favoritos = () => {
+export const Favoritos = ({navigation}) => {
     const {favoritos} = useContext(UserInfoContext);
+
+
+    function handlePressReceita(id: string) {
+        navigation.navigate('ReceitaEspecifica', {id: id});
+    }
 
     return (
         <View>
             <Header titulo="Favoritos"/>
 
             <FlatList data={favoritos} renderItem={({item}) => 
-                <ReceitaCard nome={item.title} imgUrl={item.imgUrl}/>
+                <ReceitaCard
+                    nome={item.title}
+                    imgUrl={item.imgUrl}
+                    onPress={() => handlePressReceita(item.id)}
+                />
             }/>
         </View>
     )
